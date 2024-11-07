8254d4410c7d3292ae977df842643f46c73a3132
ivonaturic <ivona.pilic.ba@gmail.com>
2024-11-07 15:07:39 +0100

    Updating employee storage

---
+++ Storage/EmployeeStorage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Employee.Common;
+using Employee.Roles;
+
+namespace Employee.Storage
+{
+    public class EmployeeStorage
+    {
+        private List<EmployeeCommon> employeelist = new List<EmployeeCommon>() 
+        {
+            new CEO("Ivona","Turić",25,0),
+            new ProjectManager("Ivan","Zeko",28,"projekt"),
+            new ProjectManager("Martina","Peric",32,"projekt2"),
+            new Developer("Petar","Perica",44,"project",false),
+            new SoftwareTester("Karla","Kartalic",44,"projekt2",true)
+        };
+
+
+        public void AddEmployees(EmployeeCommon employee) 
+        {
+            employeelist.Add(employee);
+        }
+
+        public void RemoveEmployees(EmployeeCommon employee)
+        {
+            employeelist.Remove(employee);
+        }
+
+        public IEnumerable<EmployeeCommon> AllEmployees() 
+        {
+            return employeelist;
+        }
+    }
+}
