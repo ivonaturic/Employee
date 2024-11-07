f0d17e73b9d8def0f5316eea6a2f58daf0104689
ivonaturic <ivona.pilic.ba@gmail.com>
2024-11-07 08:57:01 +0100

    add employee model

---
+++ Common/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Common
+{
+    public abstract class EmployeeCommon
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public EmployeeCommon(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+        public abstract string GetInfo();
+    }
+}
