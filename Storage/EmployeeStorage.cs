06e969d10927d7842ea31a6ee88f85834d378167
ivonaturic <ivona.pilic.ba@gmail.com>
2024-11-14 09:40:56 +0100

    Applying seed data

--- Storage/EmployeeStorage.cs
+++ Storage/EmployeeStorage.cs
@@ -10,14 +10,14 @@ namespace Employee.Storage
 {
     public class EmployeeStorage
     {
-        private List<EmployeeCommon> employeelist = new List<EmployeeCommon>()
-        {
-            new CEO("Ivona","Turić",25,0),
+        private List<EmployeeCommon> employeelist = new List<EmployeeCommon>();
+        /*{
+            new CEO("Matija","Milic",25,0),
             new ProjectManager("Ivan","Zeko",28,"projekt"),
             new ProjectManager("Martina","Peric",32,"projekt2"),
             new Developer("Petar","Perica",44,"project",false),
             new SoftwareTester("Karla","Kartalic",44,"projekt2",true)
-        };
+        };*/
 
 
         public void AddEmployees(EmployeeCommon employee) 
